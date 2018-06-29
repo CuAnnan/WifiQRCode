c531733ec5a5e67e2f04a5ac791475d9a165f242
Wing <eamonn.kearns@so-4pt.net>
2018-06-29 21:34:40 +0100

    initial commit

---
+++ js/qrCodify.js
@@ -0,0 +1,26 @@
+(
+    ()=>{
+        //WIFI:T:WPA;S:mynetwork;P:mypass;;
+        let $ssid = $('#ssid'),
+            $security = $('#security'),
+            $password = $('#password'),
+            $qrCodes = $('#qrCodes');
+        
+        $('#submitButton').click(
+            ()=>{
+                let string = `WIFI:T:${$security.val()};S:${$ssid.val()};P:${$password.val()};;`;
+                QRCode.toDataURL(string).then(
+                    (dataURL)=>{
+                        $('<li/>').appendTo($qrCodes)
+                            .append(
+                                $(`<h2/>`).text($ssid.val())
+                            )
+                            .append(
+                                $('<img/>').attr('src', dataURL).addClass('qrCode')
+                            );
+                    }
+                );
+            }
+        );
+    }
+)();
